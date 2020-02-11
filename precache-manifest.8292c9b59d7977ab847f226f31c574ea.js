159cedd21ebbfe27daf67eeafbbeb0f2aac56d21
campos514 <42255546+campos514@users.noreply.github.com>
2020-02-10 23:41:14 -0500

    Updates

---
+++ precache-manifest.8292c9b59d7977ab847f226f31c574ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb6eb53fd7986b4d58a7254a914afffa",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "b7515e3aad179c000639",
+    "url": "/monsters/static/css/main.a4304646.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7515e3aad179c000639",
+    "url": "/monsters/static/js/main.76866638.chunk.js"
+  },
+  {
+    "revision": "27edc721a646a18481ad",
+    "url": "/monsters/static/js/runtime-main.9b332845.js"
+  }
+]);
